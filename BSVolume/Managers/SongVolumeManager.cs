e3b009cc5968aed49cde1fcb39372c5499e6e801
popcorn <popcorn9499@gmail.com>
2022-07-04 13:56:01 -0500

    cleanup and actual working in game song volume setting

--- BSVolume/Managers/SongVolumeManager.cs
+++ BSVolume/Managers/SongVolumeManager.cs
@@ -10,12 +10,14 @@ namespace BSVolume.Managers
         private readonly GameplayCoreSceneSetupData _sceneSetupData;
         private readonly AudioTimeSyncController _audioTimeSyncController;
         private readonly SiraLog _log;
+        private readonly Config _config;
 
-        public SongVolumeManager(GameplayCoreSceneSetupData sceneSetupData, AudioTimeSyncController audioTimeSyncController, SiraLog log)
+        public SongVolumeManager(GameplayCoreSceneSetupData sceneSetupData, AudioTimeSyncController audioTimeSyncController, SiraLog log, Config config)
         {
             this._sceneSetupData = sceneSetupData;
             this._audioTimeSyncController = audioTimeSyncController;
             this._log = log;
+            this._config = config;
         }
 
         public void Initialize()
@@ -26,7 +28,7 @@ namespace BSVolume.Managers
             _log.Info("Level Path: " + customLevel.customLevelPath);
             var src = _audioTimeSyncController.GetComponent<AudioSource>();
             _log.Info("Original Volume: " + src.volume);
-            src.volume = 0.2f;
+            src.volume = _config.songVolume;
             _log.Info("Set volume to -1.2db, " + src.volume);
 
         }
--- BSVolume/Views/VolumeMenuView.cs
+++ BSVolume/Views/VolumeMenuView.cs
@@ -19,12 +19,14 @@ namespace BSVolume.Views
         private AudioManagerSO _audioManager;
         private MainSettingsModelSO _mainSettingsModelSO;
         private AudioTimeSyncController _audioTimeSyncController;
+        private Config _config;
         [Inject]
-        public void Construct(SiraLog log, AudioManagerSO audioManager, MainSettingsModelSO mainSettingsModelSO)
+        public void Construct(SiraLog log, AudioManagerSO audioManager, MainSettingsModelSO mainSettingsModelSO, Config config)
         {
             _log = log;
             _audioManager = audioManager;
             _mainSettingsModelSO = mainSettingsModelSO;
+            _config = config;
             //_audioTimeSyncController = audioTimeSyncController;
         }
 
@@ -37,32 +39,8 @@ namespace BSVolume.Views
         {
             gameVolume = value;
             Plugin.Log.Info($"game-value value applied, now: {gameVolume}");
-            
-            //min volume
-            /*
-            _audioManager.musicVolume = -30f;
-            _audioManager.mainVolume = -30f;
-            _audioManager.sfxVolume = -30f;
-            */
-            //Plugin.Log.Info(AudioHelpers.NormalizedVolumeToDB(value).ToString());
-            //_audioManager.musicVolume = 4f;
-            //_audioManager.mainVolume = AudioHelpers.NormalizedVolumeToDB(value);
-            /*
-            Plugin.Log.Info("Prev Value " + _mainSettingsModelSO.volume.value.ToString());
-            _mainSettingsModelSO.volume.value = value;
-            _mainSettingsModelSO.Save();
-            _mainSettingsModelSO.Load(true);
-            Plugin.Log.Info("Post Value " + _mainSettingsModelSO.volume.value.ToString());
-            */
-            
-            //_audioManager.sfxVolume = 4f;
 
-            /*
-            var src = _audioTimeSyncController.GetComponent<AudioSource>();
-            _log.Info("Original Volume: " + src.volume);
-            src.volume = value;
-            _log.Info("Set volume to "+ value + " is? " + src.volume);
-            */
+            _config.songVolume = value;
         }
 
         [UIAction("#post-parse")]
