c1d3d223dd50927bd99557d5106d9d48b5fe47d2
popcorn <popcorn9499@gmail.com>
2022-07-08 23:34:29 -0500

    fixed the beatsaber startup crash due to setting menu abience volume
    might want to make this slightly better in the future

--- BSVolume/Managers/MenuVolumeManager.cs
+++ BSVolume/Managers/MenuVolumeManager.cs
@@ -37,7 +37,15 @@ namespace BSVolume.Managers
         public void SetMenuAmbienceVolume(float volume)
         {
             var audioClip = DefaultAudioClip(ref _songPreviewPlayer);
-            _songPreviewPlayer.CrossfadeTo(audioClip, AudioHelpers.NormalizedVolumeToDB(volume), Mathf.Max(Random.Range(0f, audioClip.length - 0.1f), 0f), -1f, true, null);
+            try
+            {
+                _songPreviewPlayer.CrossfadeTo(audioClip, AudioHelpers.NormalizedVolumeToDB(volume), Mathf.Max(Random.Range(0f, audioClip.length - 0.1f), 0f), -1f, true, null);
+
+            }
+            catch
+            {
+
+            }
         }
     }
 }
