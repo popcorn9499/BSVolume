3b9a1e3b50b0e3ec31f217b171c773a464556a40
popcorn <popcorn9499@gmail.com>
2022-07-09 16:24:21 -0500

    added a patch with logging to prevent a crash
    added some logging as well

--- BSVolume/Managers/MenuVolumeManager.cs
+++ BSVolume/Managers/MenuVolumeManager.cs
@@ -10,15 +10,16 @@ namespace BSVolume.Managers
     {
         private SongPreviewPlayer _songPreviewPlayer; //store the song preview player so we can adjust volumes in that
         private readonly Config _config;
-
+        private readonly SiraLog _log;
         private static readonly FieldAccessor<SongPreviewPlayer, float>.Accessor PreviewVolume = FieldAccessor<SongPreviewPlayer, float>.GetAccessor("_volumeScale"); //get the regular volume for song previews
         private static readonly FieldAccessor<SongPreviewPlayer, AudioClip>.Accessor DefaultAudioClip = FieldAccessor<SongPreviewPlayer, AudioClip>.GetAccessor("_defaultAudioClip"); //adjust the main default audio clip at menu
         
 
-        public MenuVolumeManager(Config config, SongPreviewPlayer songPreviewPlayer)
+        public MenuVolumeManager(SiraLog log, Config config, SongPreviewPlayer songPreviewPlayer)
         {
             _config = config;
             _songPreviewPlayer = songPreviewPlayer;
+            _log = log;
         }
 
         public void Initialize()
@@ -37,14 +38,14 @@ namespace BSVolume.Managers
         public void SetMenuAmbienceVolume(float volume)
         {
             var audioClip = DefaultAudioClip(ref _songPreviewPlayer);
-            try
+            try //catching the nullpointerexception caused by setting this too early
             {
                 _songPreviewPlayer.CrossfadeTo(audioClip, AudioHelpers.NormalizedVolumeToDB(volume), Mathf.Max(Random.Range(0f, audioClip.length - 0.1f), 0f), -1f, true, null);
-
+                _log.Info("Successfully set ambience volume!");
             }
             catch
             {
-
+                _log.Debug("Failed to set ambience volume!");
             }
         }
     }
